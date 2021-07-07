631dac5f8b258791111b4a3272f9576e8cc21d70
danieljpadilla2011 <85798282+danieljpadilla2011@users.noreply.github.com>
2021-07-06 19:09:12 -0500

    Create Main.java

---
+++ Semana2/julio62021/ProjectThis/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Circulo.PI);
+        System.out.println(Circulo.calcularArea(5.0f));
+        //Circulo circulo1=new Circulo(5.0f);
+        
+        //System.out.println(circulo1.calcularArea());
+        //System.out.println(circulo1.PI);
+    }
+    
+}
