71be5079387ef8a9de3f3a27ddccbc54df51d8e8
danieljpadilla2011 <85798282+danieljpadilla2011@users.noreply.github.com>
2021-07-06 20:02:02 -0500

    Create Persona.java

---
+++ Semana2/julio62021/ProjectHerencia/Persona.java
@@ -0,0 +1,35 @@
+public class Persona {
+    protected String nombre;
+    public int edad;
+    public int documentoId;
+
+    public void mostrarEdad(){
+        System.out.println("La edad es: "+edad);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDocumentoId() {
+        return documentoId;
+    }
+
+    public void setDocumentoId(int documentoId) {
+        this.documentoId = documentoId;
+    }
+
+    
+}
