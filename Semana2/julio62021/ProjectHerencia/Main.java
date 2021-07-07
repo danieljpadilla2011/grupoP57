89b3f836b9cc805dcb263c0d4f5acad6b607f4d2
danieljpadilla2011 <85798282+danieljpadilla2011@users.noreply.github.com>
2021-07-06 20:02:47 -0500

    Create Main.java

---
+++ Semana2/julio62021/ProjectHerencia/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Estudiante estudiante1=new Estudiante("Maria", 28, 12345, 88825);
+        estudiante1.mostrarEdad();
+        System.out.println(estudiante1.getNumeroCarnet());
+    }
+    
+}
